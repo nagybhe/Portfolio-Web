958afc32091baeb2bc1e7c043d6d1ab9bf7bcf0e
Nagybhe Hage <65202520+nagybhe@users.noreply.github.com>
2021-09-08 18:59:06 -0300

    Atualização - Templete

---
+++ index.php
@@ -0,0 +1,309 @@
+ <!DOCTYPE html>
+ <html lang="pt-br">
+
+ <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <title>Nᴀɢʏʙʜᴇ Hᴀɢᴇ</title>
+   <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+   <!-- Icon -->
+   <link rel="stylesheet" href="assets/fonts/line-icons.css">
+   <!-- Owl carousel -->
+   <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+   <link rel="stylesheet" href="assets/css/owl.theme.css">
+
+   <!-- Animate -->
+   <link rel="stylesheet" href="assets/css/animate.css">
+   <!-- Main Style -->
+   <link rel="stylesheet" href="assets/css/main.css">
+   <!-- Responsive Style -->
+   <link rel="stylesheet" href="assets/css/responsive.css">
+
+ </head>
+
+ <body>
+
+   <!-- Header Area wrapper Starts -->
+   <header id="header-wrap">
+     <!-- Navbar Start -->
+     <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
+       <div class="container">
+         <!-- Brand and toggle get grouped for better mobile display -->
+         <a href="index.php" class="navbar-brand"></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+           <i class="lni-menu"></i>
+         </button>
+         <div class="collapse navbar-collapse " id="navbarCollapse ">
+           <ul class="navbar-nav mr-auto w-100 justify-content-end clearfix">
+
+             <li class="nav-item">
+               <a class="nav-link" href="#services">
+                 TRABALHOS
+               </a>
+             </li>
+           </ul>
+         </div>
+       </div>
+     </nav>
+     <!-- Navbar End -->
+     <br><br><br>
+     <!-- Hero Area Start -->
+     <div class="hero-area-bg ">
+       <div class="container">
+         <div class="row">
+           <div class="col-lg-7 col-md-12 col-sm-12 col-xs-12 col-md-6 offset-md-3">
+             <div class="contents">
+               <h2 class="head-title text-center">André Nagybhe Hage Ramos</h2>
+               <p class="text-center">
+                 Sou formado em Sistemas para Internet (Faculdade de Tecnoliga do Amapá), Amante de E-sports, meu jogo favorito é League Of Legends.
+                 Uma pessoa firme e segura sobre minhas decisões. Possuo muita energia, rapidez de raciocínio e necessidade de realização.
+                 Posso ser ambicioso e competitivo, agindo de forma focada e determinada para conquistar os meus objetivos.
+               </p><br>
+               <p class="text-center"><strong>Front-End and Back-End | Web Design | User Experience and Interface Design |</strong> </p>
+               <div class="widget text-center">
+                 <div class="social-icon">
+                   <a class="instagram" href="https://www.instagram.com/nagybhe_/"><i class="lni-instagram-filled"></i></a>
+                   <a class="linkedin" href="https://www.linkedin.com/in/andr%C3%A9-nagybhe-153b171b2/"><i class="lni-linkedin-filled"></i></a>
+
+                 </div>
+               </div>
+             </div>
+           </div>
+
+         </div>
+       </div>
+     </div>
+
+     <!-- Hero Area End -->
+
+   </header>
+   <br>
+   <!-- Header Area wrapper End -->
+   <hr style="border-width: 1px 1px 0; border-style: solid; border-color: palevioletred; width: 50%; margin-left: auto; margin-right: auto;">
+   <!-- Services Section Start -->
+   <section id="services" class="section-padding">
+
+     <div class="container">
+       <div class="section-header text-center">
+         <h2 class="section-title wow fadeInDown text-center" data-wow-delay="0.3s">Trabalhos</h2>
+         <div class="shape wow fadeInDown" data-wow-delay="0.3s"></div>
+       </div>
+       <div class="row">
+         <!-- Services item -->
+         <div class="col-md-6 col-lg-4 col-xs-12">
+           <div class="services-item wow fadeInRight" data-wow-delay="0.3s">
+             <div class="icon">
+               <i class="lni-cog"></i>
+             </div>
+             <div class="services-content">
+               <h3><a href="https://github.com/nagybhe">GITHUB</a></h3>
+               <p>Dentre esses projetos existem outros no GithHub que você pode dar uma observada</p>
+             </div>
+           </div>
+         </div>
+         <!-- Services item -->
+
+         <!-- Services item -->
+
+         <!-- figma -->
+         <div class="col-md-6 col-lg-4 col-xs-12">
+           <div class="services-item wow fadeInRight" data-wow-delay="1.2s">
+             <div class="icon">
+               <i class="lni-layers"></i>
+             </div>
+             <div class="services-content">
+               <h3><a href="https://www.figma.com/file/t8EPY2hIdy2OmKTcjryKE4/INTERFACE-MOBILE?node-id=0%3A1">Interface Mobile</a></h3>
+               <p>Protótipo Mobile servindo de modelo ou molde para futuras produções ou possíveis melhorias, utilizando o figma.</p>
+             </div>
+           </div>
+         </div>
+         <!-- react -->
+         <div class="col-md-6 col-lg-4 col-xs-12">
+           <div class="services-item wow fadeInRight" data-wow-delay="1.5s">
+             <div class="icon">
+               <i class="lni-mobile"></i>
+             </div>
+             <div class="services-content">
+               <h3><a href="https://snack.expo.io/@ins4nityhz/6bc662">Projeto em React Native </a></h3>
+               <p><strong>Open Source ❤️</strong></p>
+               <p>Para Desenvolver utilizei Snack expo, visual studio code, leia o README.md</p>
+             </div>
+           </div>
+         </div>
+         <!-- react -->
+         <div class="col-md-6 col-lg-4 col-xs-12">
+           <div class="services-item wow fadeInRight" data-wow-delay="1.5s">
+             <div class="icon">
+               <i class="lni-mobile"></i>
+             </div>
+             <div class="services-content">
+               <h3><a href="https://snack.expo.io/@ins4nityhz/4d20c3">Projeto em React Native</a></h3>
+               <p><strong>Open Source ❤️</strong></p>
+               <p>Para Desenvolver utilizei Snack expo, visual studio code</p>
+             </div>
+           </div>
+         </div>
+         <!-- crud -->
+         <div class="col-md-6 col-lg-4 col-xs-12">
+           <div class="services-item wow fadeInRight" data-wow-delay="1.5s">
+             <div class="icon">
+               <i class="lni-mobile"></i>
+             </div>
+             <div class="services-content">
+               <h3><a href="https://github.com/nagybhe/Projeto-CRUD">Projeto CRUD</a></h3>
+               <p><strong>Open Source ❤️</strong></p>
+               <p>O projeto desenvolvido representa as quatro principais operações com bancos de dados: Criar, Ler, Atualizar e Excluir dados.</p>
+             </div>
+           </div>
+         </div>
+         <!-- siscure -->
+         <div class="col-md-6 col-lg-4 col-xs-12">
+           <div class="services-item wow fadeInRight" data-wow-delay="1.5s">
+             <div class="icon">
+               <i class="lni-mobile"></i>
+             </div>
+             <div class="services-content">
+               <h3><a href="https://github.com/nagybhe/SIS-CURE">SIS-CURE:</a></h3>
+               <p><strong>SISTEMA WEB PARA A GESTÃO DE FARMÁCIA </strong></p>
+               <p>Trabalho de Conclusão de Curso - Sistemas Para Internet - 2021</p>
+             </div>
+           </div>
+         </div>
+         <!-- Services item -->
+
+       </div>
+     </div>
+   </section>
+
+   <hr style="border-width: 1px 1px 0; border-style: solid; border-color: palevioletred; width: 50%; margin-left: auto; margin-right: auto;">
+
+   <section id="features" class="section-padding">
+     <div class="container">
+       <div class="section-header text-center">
+         <h2 class="section-title wow fadeInDown" data-wow-delay="0.3s">Minhas Skils</h2>
+
+       </div>
+       <div class="row">
+         <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+           <div class="content-left">
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.3s">
+
+               <div class="text text-center">
+                 <h4>HTML5</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.6s">
+               <div class="text text-center ">
+                 <h4>CSS3</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.9s">
+               <div class="text text-center">
+                 <h4>JAVASCRIPT</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.9s">
+               <div class="text text-center">
+                 <h4>REACT NATIVE</h4>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+           <div class="content-left">
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.3s">
+
+               <div class="text text-center">
+                 <h4>FRAMEWORK BOOTSTRAP</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.6s">
+               <div class="text text-center ">
+                 <h4>PHP</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.9s">
+               <div class="text text-center">
+                 <h4>BANCO DE DADOS</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.9s">
+               <div class="text text-center">
+                 <h4>GIT</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInLeft" data-wow-delay="0.9s">
+               <div class="text text-center">
+                 <h4>GERÊNCIA DE PROJETOS</h4>
+               </div>
+             </div>
+           </div>
+         </div>
+         <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+           <div class="content-right">
+             <div class="box-item wow fadeInRight" data-wow-delay="0.3s">
+               <div class="text text-center">
+                 <h4>MARKDOWN</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInRight" data-wow-delay="0.6s">
+
+               <div class="text text-center">
+                 <h4>ADOBE ILLUSTRATOR</h4>
+
+               </div>
+             </div>
+             <div class="box-item wow fadeInRight" data-wow-delay="0.9s">
+               <div class="text text-center">
+                 <h4>ADOBE PHOTOSHOP</h4>
+               </div>
+             </div>
+             <div class="box-item wow fadeInRight" data-wow-delay="0.9s">
+               <div class="text text-center">
+                 <h4>FIGMA</h4>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </section>
+
+   <hr style="border-width: 1px 1px 0; border-style: solid; border-color: palevioletred; width: 50%; margin-left: auto; margin-right: auto;">
+   <!-- Contact Section End -->
+
+   <!-- Footer Section Start -->
+
+   <!-- Footer Section End -->
+
+   <!-- Go to Top Link -->
+   <a href="#" class="back-to-top">
+     <i class="lni-arrow-up"></i>
+   </a>
+
+   <!-- Preloader -->
+   <div id="preloader">
+     <div class="loader" id="loader-1"></div>
+   </div>
+   <!-- End Preloader -->
+
+   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="assets/js/jquery-min.js"></script>
+   <script src="assets/js/popper.min.js"></script>
+   <script src="assets/js/bootstrap.min.js"></script>
+   <script src="assets/js/owl.carousel.min.js"></script>
+   <script src="assets/js/wow.js"></script>
+   <script src="assets/js/jquery.nav.js"></script>
+   <script src="assets/js/scrolling-nav.js"></script>
+   <script src="assets/js/jquery.easing.min.js"></script>
+   <script src="assets/js/main.js"></script>
+   <script src="assets/js/form-validator.min.js"></script>
+   <script src="assets/js/contact-form-script.min.js"></script>
+
+ </body>
+
+ </html>
